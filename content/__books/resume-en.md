050d1c8dbfcc66c860fa6c9784ed4d700b6bcc62
goodzzong <goodzzong@gmail.com>
2019-08-28 10:06:22 +0900

    create books.md & add til posts

---
+++ content/__books/resume-en.md
@@ -0,0 +1,11 @@
+---
+title: 'books'
+date: 2019-1-27 16:21:13
+lang: 'ko'
+---
+
+# Goodzzong's Book
+
+<div align="center">
+
+</div>
--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/content/__books`,
+        name: `books`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
       },
